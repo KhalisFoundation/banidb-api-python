74e25bd595c704e1d66f68731fbe72b3839ccbdc
Gurmehar Singh Khalsa <gurmeharsinghkhalsa1005@gmail.com>
2021-05-25 18:26:43 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from setuptools import setup
+
+with open('README.md', 'r') as f:
+    README = f
+
 setup(
     name='banidb',
-    packages=['banidb'],
     version='0.1',
     license='MIT',
     description='BaniDB API for Python',
+    long_description=README,
+    long_description_content_type='text/markdown',
     author='Khalis Foundation',
     url='https://github.com/KhalisFoundation/banidb-api-python',
-    keywords=['BaniDB', 'API', 'Amrit Keertan', 'Sundar Gutka', 'SikhiToTheMax', 'Gurbani', 'KhalisFoundation', 'Sikh', 'Guru Granth Sahib'],
+    keywords=[
+        'BaniDB',
+        'SikhiToTheMax',
+        'Gurbani',
+        'KhalisFoundation',
+        'Sikh',
+        'Guru Granth Sahib',
+        'API',
+        'Amrit Keertan',
+        'Sundar Gutka'
+    ],
     install_requires=['requests'],
     author_email='',
     download_url='',
@@ -22,6 +37,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
+    packages=['banidb'],
+    include_package_data=True,
     project_urls={
         'Documentation': '',
         'Source': 'https://github.com/KhalisFoundation/banidb-api-python',
