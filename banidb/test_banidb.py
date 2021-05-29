bdeb18cd8c3bb14697592a2b08a380ef1534d859
Gurmehar Singh Khalsa <gurmeharsinghkhalsa1005@gmail.com>
2021-05-29 09:56:31 +0530

    Updated mock tests

--- banidb/test_banidb.py
+++ banidb/test_banidb.py
@@ -1,33 +1,27 @@
 # -*- coding: utf-8 -*-
 from unittest.mock import Mock, patch
 import requests
-import banidb.banidb
-
-
-def backend_up():
-    js = requests.get('https://api.banidb.com/v2/health').json()
-    if js is True:
-        return True
-    else:
-        return False
+import banidb
 
 
 @patch('banidb.banidb.requests.get')
 def test_backend_up(mock_get):
-    mock_get.return_value.ok = True
+    def backend_up():
+        js = requests.get('https://api.banidb.com/v2/health').json()
+        if js['ok'] is True:
+            return True
+        else:
+            return False
+    true = {'ok': True}
+    mock_get.return_value = Mock(ok=True)
+    mock_get.return_value.json.return_value = true
     response = backend_up()
-    assert response is False
-
-
-def mock_search(query, searchtype=1, source='all', larivaar=False,
-                ang=None, raag=None, writer='all', page=1, results=None):
-    return banidb.search(query, searchtype, source, larivaar,
-                         ang, raag, writer, page, results)
+    assert response is True
 
 
 @patch('banidb.banidb.requests.get')
 def test_search(mock_get):
-    search_res = mock_search('nhknknkss')
+    search_res = banidb.search('jjcAhdh')
     mock_get.return_value = Mock(ok=True)
     mock_get.return_value.json.return_value = search_res
     result = requests.get()
