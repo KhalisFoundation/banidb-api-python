83bad66aa8fafc8ebdb16cdd0b91def77f5771ab
Balwinder Kaur <bkaurca@gmail.com>
2021-07-08 15:08:25 -0400

    manual version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='banidb',
-    version='0.3.1',
+    version='0.3.2',
     license='MIT',
     description='Python Package for Sikh Gurbani - BaniDB API',
     long_description=README,
